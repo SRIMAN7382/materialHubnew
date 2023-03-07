48516aac897809b1f8a25a28a86ec70436fe9608
SaiDeepak Goud Depally <117894356+SaiDeepak16@users.noreply.github.com>
2023-03-07 09:37:59 +0530

    Add files via upload

---
+++ MyJS/nav2.js
@@ -0,0 +1,15 @@
+const btn_menu = document.querySelector(".btn-menu");
+      const side_bar = document.querySelector(".sidebar");
+
+      btn_menu.addEventListener("click", function () {
+        side_bar.classList.toggle("expand");
+        changebtn();
+      });
+
+      function changebtn() {
+        if (side_bar.classList.contains("expand")) {
+          btn_menu.classList.replace("bx-menu", "bx-menu-alt-right");
+        } else {
+          btn_menu.classList.replace("bx-menu-alt-right", "bx-menu");
+        }
+      }
